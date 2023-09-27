aa21d332c21b21aabffd7c62e150d4f8a463a1d8
José <91501518+Jmlucero1984@users.noreply.github.com>
2023-09-26 23:58:44 -0300

    Add new classes (#3)
    * Added a line
    
    * Added a line
    Added a class
    Added a folder
    
    * Added a line
    Added a class
    Added a folder
    
    * Add a Perro
    Add a abstract Animal
    
    ---------
    
    Co-authored-by: Jose Ma Lucero <ingenierolucero84@gmail.com>
    Co-authored-by: Jmlucero1984 <>

---
+++ MartesNocheGitHub/src/main/java/com/jml/martesnochegithub/Animal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jml.martesnochegithub;
+
+/**
+ *
+ * @author Ing. J.M.Lucero
+ */
+public abstract class Animal {
+    protected String nombre;
+    protected Animal() {
+        
+    }
+    
+    protected Animal (String nombre) {
+        this.nombre = nombre;
+    }
+        
+    @Override 
+    public String toString() {
+        return "Hello i am a "+ this.getClass().getName()+" y me llamo "+nombre;
+    }
+}
