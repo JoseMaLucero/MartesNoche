58092a95d5aa1fe6e516d052abd484dfdb9d8148
ingenierolucero84 <ingenierolucero84@gmail.com>
2023-09-27 02:36:03 +0100

    create java project

---
+++ MartesNocheGitHub/src/main/java/com/jml/martesnochegithub/MartesNocheGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jml.martesnochegithub;
+
+/**
+ *
+ * @author jmlucero
+ */
+public class MartesNocheGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
