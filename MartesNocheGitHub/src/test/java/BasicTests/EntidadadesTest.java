71ddb1cd5942bbf9728533972aec13433551fb53
ingenierolucero84 <ingenierolucero84@gmail.com>
2023-09-27 04:04:22 +0100

    add test package

---
+++ MartesNocheGitHub/src/test/java/BasicTests/EntidadadesTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package BasicTests;
+
+import com.jml.martesnochegithub.Animal;
+import com.jml.martesnochegithub.Perro;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author jmlucero
+ */
+public class EntidadadesTest {
+    
+    public EntidadadesTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+      @Test
+      public void basicTest() {
+          Perro perro = new Perro();
+          assertTrue(perro instanceof Animal);
+      }
+}
