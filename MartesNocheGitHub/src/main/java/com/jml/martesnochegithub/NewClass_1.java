2cfc26173249d152d850236b4e65199281d74c93
Jose Ma Lucero <ingenierolucero84@gmail.com>
2023-09-26 23:27:35 -0300

    Added a line Added a class Added a folder

---
+++ MartesNocheGitHub/src/main/java/com/jml/martesnochegithub/NewClass_1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jml.martesnochegithub;
+
+/**
+ *
+ * @author Ing. J.M.Lucero
+ */
+public class NewClass_1 {
+    
+}
