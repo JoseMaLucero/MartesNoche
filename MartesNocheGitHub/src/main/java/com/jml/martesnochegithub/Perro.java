e3d606207a322afba2ee20c09b5279f66ea0d140
Jmlucero1984 <>
2023-09-26 23:49:14 -0300

    Add a Perro Add a abstract Animal

---
+++ MartesNocheGitHub/src/main/java/com/jml/martesnochegithub/Perro.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jml.martesnochegithub;
+
+/**
+ *
+ * @author Ing. J.M.Lucero
+ */
+public class Perro extends Animal {
+    String nombre;
+    public Perro() {};
+    public Perro(String nombre) {
+        super(nombre);
+    }
+    
+ 
+    
+    
+}
