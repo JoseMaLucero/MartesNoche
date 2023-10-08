86f8ebff7ea25afd308cce99a00e3bde81b14df4
ingenierolucero84 <ingenierolucero84@gmail.com>
2023-10-08 16:03:13 -0300

    Fix the assert

--- MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
+++ MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
@@ -42,6 +42,6 @@ public class TestVacio {
           String name = "Cachito";
           Perro perro = new Perro(name);
           String expectedOutput = "Hello i am a "+perro.getClass().getSimpleName()+" y me llamo "+name;
-          assertNotEquals(expectedOutput,perro.toString());
+          assertEquals(expectedOutput,perro.toString());
       }
 }
