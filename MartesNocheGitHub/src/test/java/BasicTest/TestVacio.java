2250224b03ff48aa64445cbef8fddd1ab56025d2
ingenierolucero84 <ingenierolucero84@gmail.com>
2023-10-08 15:25:14 -0300

    --

--- MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
+++ MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
@@ -42,6 +42,6 @@ public class TestVacio {
           String name = "Cachito";
           Perro perro = new Perro(name);
           String expectedOutput = "Hello i am a "+perro.getClass().getSimpleName()+" y me llamo "+name;
-          assertFalse(expectedOutput,perro.toString());
+          assertTrue(expectedOutput,perro.toString());
       }
 }
