2495469ac7dadc37750d8be4fdec867a905b9b0b
ingenierolucero84 <ingenierolucero84@gmail.com>
2023-10-08 16:50:22 -0300

    Fix the assert

--- MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
+++ MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
@@ -42,7 +42,7 @@ public class TestVacio {
           String name = "Cachito";
           Perro perro = new Perro(name);
           String expectedOutput = "Hello i am a "+perro.getClass().getSimpleName()+" y me llamo "+name;
-          assertFalse(expectedOutput,perro.toString()); //
+          assertFalse(expectedOutput,perro.toString()); // cacac
 
       }
 }
