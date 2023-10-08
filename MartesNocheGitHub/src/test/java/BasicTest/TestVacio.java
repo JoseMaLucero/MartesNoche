612d5be760e4bb703c573c828e5bb4ac91b90704
Jmlucero1984 <jmlucero1984@gmail.com>
2023-10-08 18:21:23 -0300

    Merge origin/main into main
    Conflicts:
    	MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java

--- MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
+++ MartesNocheGitHub/src/test/java/BasicTest/TestVacio.java
@@ -42,7 +42,7 @@ public class TestVacio {
           String name = "Cachito";
           Perro perro = new Perro(name);
           String expectedOutput = "Hello i am a "+perro.getClass().getSimpleName()+" y me llamo "+name;
-         assertEquals(expectedOutput,perro.toString()); // casdfssdasdsadjgukjhsdssdsdfsdffsdfdvsdasdasdasaddfjy dasdfas465454cac464
+         assertFalse(expectedOutput,perro.toString()); // casdfssdasdsadjgukjhsdssdsdfsdffsdfdvsdasdasdasaddfjy dasdfas465454cac464
 
       }
 }
